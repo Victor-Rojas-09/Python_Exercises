c3bd0817b40107e401bd3d7b654e8e60fe5e15eb
rojas <victor.rojas@utp.edu.co>
2026-03-13 22:09:58 -0500

    Add the visualisation of the other method to Steeping Stone.

--- Transport_Problem/__main__.py
+++ Transport_Problem/__main__.py
@@ -15,7 +15,7 @@ from optimization.stepping_stone import SteppingStone
 from visualization.visualizer import TransportVisualizer
 
 def main():
-    # Datos del problema (ejemplo)
+    # Datos del problema
     costs = np.array([
         [5, 2, 7, 3],
         [3, 6, 6, 1],
@@ -30,32 +30,43 @@ def main():
     problem = TransportProblem(costs, supply, demand)
     problem.balance_problem()
 
-    # Método 1: Esquina Noroeste
+    # Metodo de la Esquina Noroeste
     nw_method = NorthWestCorner()
     nw_solution = nw_method.solve(problem)
 
-    # Método 2: Costo Mínimo
+    # Metodo del Costo Mínimo
     mc_method = MinimumCostMethod()
     mc_solution = mc_method.solve(problem)
 
-    # Método 3: Vogel
+    # Metodo de Vogel
     vg_method = VogelApproximation()
     vg_solution = vg_method.solve(problem)
 
-    # Optimización con Stepping Stone (desde NW)
+    # Optimizacion con Stepping Stone
+    # Para la Esquina Noroeste
     optimizer = SteppingStone()
-    optimal_solution, history = optimizer.optimize(nw_solution, problem)
+    optimal_solution_NW, history = optimizer.optimize(nw_solution, problem)
 
-    # Visualización de resultados
+    # Para Costo Minimo
+    optimizer = SteppingStone()
+    optimal_solution_MC, history = optimizer.optimize(mc_solution, problem)
+
+    # Para Vogel
+    optimizer = SteppingStone()
+    optimal_solution_VG, history = optimizer.optimize(vg_solution, problem)
+
+    # Visualizacion de resultados
     visualizer = TransportVisualizer()
     visualizer.show_assignment(nw_solution, problem, "Asignación — Esquina Noroeste")
     visualizer.show_assignment(mc_solution, problem, "Asignación — Costo Mínimo")
     visualizer.show_assignment(vg_solution, problem, "Asignación — Vogel")
-    visualizer.show_assignment(optimal_solution, problem, "Asignación Óptima - Stepping Stone")
+    visualizer.show_assignment(optimal_solution_NW, problem, "Asignación Óptima para Esquina Noroeste - Stepping Stone")
+    visualizer.show_assignment(optimal_solution_MC, problem, "Asignación Óptima para Costo Mínimo - Stepping Stone")
+    visualizer.show_assignment(optimal_solution_VG, problem, "Asignación Óptima Para Vogel- Stepping Stone")
 
-    # Gráficos
+    # Graficos
     visualizer.plot_convergence(history)
-    visualizer.compare_solutions(nw_solution, optimal_solution, problem)
+    visualizer.compare_solutions(nw_solution, optimal_solution_VG, problem)
 
 if __name__ == "__main__":
     main()
